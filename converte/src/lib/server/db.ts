8b09e11e6a9fdba5b53d6ade57f79fd2cd6a1bfc
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2026-04-17 09:28:52 -0300

    Update para o servidor local

--- converte/src/lib/server/db.ts
+++ converte/src/lib/server/db.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@libsql/client';
 
 export const db = createClient({
-    url: `file:${process.env.DATABASE_PATH ?? './data/banco_local_pei.db'}`
+    url: `file:${process.env.DATABASE_PATH ?? './banco_local_pei.db'}`
 });
