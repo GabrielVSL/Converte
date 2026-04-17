2c96929d9d40b7999e7141fbdcedffffa78eda9b
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2026-04-17 11:02:20 -0300

    Build do contaier ajustado

--- converte/src/lib/server/db.ts
+++ converte/src/lib/server/db.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@libsql/client';
+import { building } from '$app/environment'; // Importa o detector de build
 
+// Se estiver "buildando", usa a memória RAM. Se estiver rodando no servidor, usa o arquivo real.
 export const db = createClient({
-    url: `file:${process.env.DATABASE_PATH ?? './banco_local_pei.db'}`
+    url: building ? 'file::memory:' : `file:${process.env.DATABASE_PATH ?? './banco_local_pei.db'}`
 });
